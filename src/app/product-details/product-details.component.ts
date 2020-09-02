87caab9bf09ce7b8832dc96f94f1cb277ee3bbc1
oletendart <letendartolivia@gmail.com>
2020-09-02 19:46:12 +0200

    Modificate files product-details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
 
@@ -11,13 +11,14 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor( private route: ActivatedRoute, ) { 
-  }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')];
-    });
+     this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productId')];
+  });
   }
 
 }
