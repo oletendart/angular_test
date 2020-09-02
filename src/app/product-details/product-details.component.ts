759b8f68b8ac5f63a3697082cff5f3b76d0940e4
oletendart <letendartolivia@gmail.com>
2020-09-02 20:09:29 +0200

    modificate div ngFor, adding "index as productId" in the file product-list.component.html

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -16,8 +16,8 @@ export class ProductDetailsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-     this.route.paramMap.subscribe(params => {
-    this.product = products[+params.get('productId')];
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
   });
   }
 
