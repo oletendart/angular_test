afcd59ffb72ac9e1a9615f41011dbdbeea4539d4
oletendart <letendartolivia@gmail.com>
2020-09-03 17:16:43 +0200

    create array empty + create 3 functions (add, get and clear) in the class CartService in the file cart.service.ts

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -4,7 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
+  items = [];
+  
+  addToCart(product) {
+    this.items.push(product);
+  }
 
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
   constructor() { }
 
 }
