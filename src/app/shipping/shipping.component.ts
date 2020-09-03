7d91faac1855139cce99a5a36d9c5e7fcbef63cf
oletendart <letendartolivia@gmail.com>
2020-09-03 17:44:15 +0200

    Define a shippingCosts property + inject the cart service in the ShippingComponent construtor + set the property shippingCosts using the getShippingPrices method from the cart service in the file shipping.component.ts

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -8,10 +8,14 @@ import { CartService } from '../cart.service';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
