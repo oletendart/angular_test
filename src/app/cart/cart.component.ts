b72632112f5cb54af0535f1d58beb90012ffba5e
oletendart <letendartolivia@gmail.com>
2020-09-04 12:49:40 +0200

    Create function onSubmit method to process the form in teh file cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -24,5 +24,12 @@ export class CartComponent implements OnInit {
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
+  onSubmit(customerData) {
+    // Process chekout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
 
 }
